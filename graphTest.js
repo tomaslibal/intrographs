7167affec5048066a5fd31dc081d07d0c14855a4
Tomas Libal <tomas@libal.eu>
2015-02-03 21:03:02 +0000

    add graphTest with two pending tests

---
+++ graphTest.js
@@ -0,0 +1,13 @@
+/*
+ * Test the Graph class
+ */
+describe('Graph', function() {
+    "use strict";
+
+    describe('constructor', function() {
+        it('is defined and is a function');
+        it('returns a new object of Graph');
+    });
+
+});
+
