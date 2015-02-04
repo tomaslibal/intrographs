4fd9347b0cdaaefe230c632d693dc3db61f046c6
Tomas Libal <tomas@libal.eu>
2015-02-04 21:55:39 +0000

    implement the two pending consuctor tests

--- graphTest.js
+++ graphTest.js
@@ -1,12 +1,27 @@
 /*
+ * Test subject
+ */
+var Graph = require("./graph").Graph;
+
+/*
+ * Test dependencies
+ */
+var assert = require("assert");
+
+/*
  * Test the Graph class
  */
 describe('Graph', function() {
     "use strict";
 
     describe('constructor', function() {
-        it('is defined and is a function');
-        it('returns a new object of Graph');
+        it('is defined and is a function', function() {
+            assert.equal(true, "function" === typeof Graph);    
+        });
+        it('returns a new object of Graph', function() {
+            var tmp = new Graph();
+            assert.equal(true, true === tmp instanceof Graph);
+        });
     });
 
 });
