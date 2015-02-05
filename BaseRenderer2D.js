36f2a8d8870c50e4ebb58d65f1ddc2faff53d8d1
Tomas Libal <tomas@libal.eu>
2015-02-05 22:02:50 +0000

    add prototype with setCanvas function in it

--- BaseRenderer2D.js
+++ BaseRenderer2D.js
@@ -9,4 +9,12 @@ function BaseRenderer2D(args) {
     this.canvas = args.canvas || null;
 }
 
+BaseRenderer2DPrototype = {
+    setCanvas: function(canvas) {
+        "use strict";
 
+        this.canvas = canvas;
+    }
+};
+
+BaseRenderer2D.prototype = BaseRenderer2DPrototype;
