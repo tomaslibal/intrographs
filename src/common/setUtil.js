21238e549d337299310f9051399fa87e8bcaa9e5
Tomas Libal <tomas@libal.eu>
2015-02-08 14:20:25 +0000

    add setUtil with 4 utility functions

---
+++ src/common/setUtil.js
@@ -0,0 +1,44 @@
+var setUtil = {
+    /*
+     * Definition 1: a set is a collection of distinc objects none of which is
+     * the set itself.
+     *
+     */
+    isSet: function(set) {
+        "use strict";
+
+        not_implemented();
+    },
+    /*
+     * Definition 2: A set containing no elements is called a null set.
+     *
+     * Theorem 1: There is only one empty set
+     */
+    isNullSet: function(set) {
+        "use strict";
+        not_implemented();
+    },
+    /*
+     * Definition 3: set A is said to be a subset of B if every element of A
+     * is also an element of B.
+     */
+    isSubset: function(a, b) {
+        "use strict";
+        not_implemented();
+    },
+    /*
+     * Definition 4: sets A and B are said to be equal when A is a subset of B
+     * and B is a subset of A.
+     */
+    equals: function(a, b) {
+        "use strict";
+        not_implemented();
+    }
+
+};
+
+function not_implemented() {
+    "use strict";
+
+    throw new Error("Not yet implemented");
+}
