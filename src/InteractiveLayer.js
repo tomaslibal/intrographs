e53058b9f4630512d7e9d95498f1e3afa6820d88
Tomas Libal <tomas@libal.eu>
2015-02-21 14:21:11 +0000

    implement Observer.dispatch method

--- src/InteractiveLayer.js
+++ src/InteractiveLayer.js
@@ -9,8 +9,45 @@
  */
 function InteractiveLayer(graph, canvas)
 {
+    "use strict";
 
 }
+
+function Observer() {
+    "use strict";
+
+    this.listeners = [];
+}
+
+/*
+ * Suppose we have a canvas `var cnvs` and we want to perform some action 
+ * every time a certain event `click` happens. The Observer helps to do this
+ * as follows:
+ *
+ * Observer.observe(cnvs, 'click', callback);
+ * Observer.dispatch('click'); 
+ *
+ * The dispatch method invokes all registered callbacks that listen to the click
+ * event.
+ */
+Observer.prototype.observe = function(obj, eventType, callback) {
+    "use strict";
+
+    this.listeners.push({
+        eventType: eventType,
+        callback: callback
+    });
+};
+
+Observer.prototype.dispatch = function(eventType) {
+    "use strict";
+
+    for(var i = 0; i < this.listeners.length; i++) {
+        if (this.listeners[i].eventType === eventType) {
+            this.listeners[i].callback.call(undefined);
+        }
+    }
+};
 
 if ("undefined" !== typeof process && process.env) {
     module.exports = InteractiveLayer;
