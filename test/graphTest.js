f4a457cd9f3266e108e301c424aef1746b3d057a
Tomas Libal <tomas.libal@unrulymedia.com>
2015-08-25 00:10:57 +0900

    test adding vertices

--- test/graphTest.js
+++ test/graphTest.js
@@ -14,13 +14,35 @@ describe('Graph', () => {
         });
 
         it('returns a new object of Graph', () => {
-            var tmp = new Graph();
+            let tmp = new Graph();
             assert.equal(true, true === tmp instanceof Graph);
         });
 
         it('has an empty set of vertices', () => {
             let g = new Graph();
             assert.equal(0, g.vertices.length);
+            assert.equal(0, g.edges.length);
+        });
+    });
+
+    describe('vertices', () => {
+
+        let g;
+
+        beforeEach(() => {
+            g = new Graph();
+        });
+
+        it('adds a new Vertex', () => {
+            g.addVertex();
+            assert.equal(1, g.vertices.length);
+            assert.equal(0, g.edges.length);
+        });
+
+        it('adds a second Vertex', () => {
+            g.addVertex();
+            g.addVertex();
+            assert.equal(2, g.vertices.length);
             assert.equal(0, g.edges.length);
         });
     });
