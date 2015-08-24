0a6d9fd5fc833aef4dd7b2c301cebe69b4f7970a
Tomas Libal <tomas.libal@unrulymedia.com>
2015-08-24 10:50:19 +0900

    rewrite as es2015 modules

--- src/common/mathUtil.js
+++ src/common/mathUtil.js
@@ -5,22 +5,16 @@
  * In the future it should be possible to pass a math library to the Graph class
  * so that the class would use funtions from that injected library.
  */
-var mathUtil = {
+export default class MathUtil = {
     /*
-     * Function returns an integer number from within the given range [min, 
-     * max]. 
+     * Function returns an integer number from within the given range [min,
+     * max].
      *
      * The random generator is the JavaScript random generator. This one does
      * not seem to include seed function.
      *
      */
-    getRandomArbitrary: function(min, max) {
-        "use strict";
-
+    getRandomArbitrary(min=0, max=65536) {
         return Math.ceil((Math.random() * (max - min))) + min;
     }
 };
-
-if ("undefined" !== typeof process && process.env) {
-    module.exports = math;
-}
--- src/common/setUtil.js
+++ src/common/setUtil.js
@@ -1,54 +1,40 @@
-var setUtil = {
+let not_implemented = (fncName) => {
+    throw new Error(`${fncName}: Not yet implemented!`);
+}
+
+export default class SetUtil {
     /*
      * Definition 1: a set is a collection of distinc objects none of which is
      * the set itself.
      *
      */
-    isSet: function(set) {
-        "use strict";
-
+    static isSet(set) {
         not_implemented("setUtil.isSet");
-    },
+    }
+
     /*
      * Definition 2: A set containing no elements is called a null set.
      *
      * Theorem 1: There is only one empty set
      */
-    isNullSet: function(set) {
-        "use strict";
+    static isNullSet(set) {
         not_implemented("setUtil.isNullSet");
-    },
+    }
+
     /*
      * Definition 3: set A is said to be a subset of B if every element of A
      * is also an element of B.
      */
-    isSubset: function(a, b) {
-        "use strict";
+    static isSubset(a, b) {
         not_implemented("setUtil.isSubset");
-    },
+    }
+
     /*
      * Definition 4: sets A and B are said to be equal when A is a subset of B
      * and B is a subset of A.
      */
-    equals: function(a, b) {
-        "use strict";
+    static equals(a, b) {
         not_implemented("setUtil.equals");
     }
 
-};
-
-function not_implemented() {
-    "use strict";
-        
-    var str = "Not yet implemented!";
-          
-    if (fcn) {
-        str = fcn + ": " + str;
-    }
-             
-    if (msg) {
-        str += " " + msg;
-    }
-                  
-    throw new Error(str);
 }
