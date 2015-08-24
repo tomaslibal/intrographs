5838d3b9f843db3a19d3d7a5fc971599e0097305
Tomas Libal <tomas.libal@unrulymedia.com>
2015-08-25 00:45:19 +0900

    add renderable vertex representation

---
+++ src/presentation/VertexRenderable.js
@@ -0,0 +1,20 @@
+import IRenderable from "./IRenderable";
+import assert from "../common/assert";
+
+export default class VertexRenderable extends IRenderable {
+
+    constructor({ 'posX': x, 'posY': y }) {
+
+        super({ 'posX': x, 'posY': y });    
+    }
+
+    render(graphRenderer) {
+        
+        let ctx = graphRenderer.getContext(graphRenderer.canvas);
+
+        assert(ctx != 1);
+
+        graphRenderer.paint.dot(ctx, this.x, this.y);
+    }
+
+}
