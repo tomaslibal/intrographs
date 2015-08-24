65a972892ad774924ba3b1aa4c9daf038fe13696
Tomas Libal <tomas.libal@unrulymedia.com>
2015-08-24 10:31:48 +0900

    add empty module Observer

---
+++ src/common/Observer.js
@@ -0,0 +1,13 @@
+export default class Observer {
+
+    // observer is created from an object which acts as a source of events
+    constructor(from, name='untitle observer') {
+
+    }
+
+    // subscribe, dispose to certain events on the underlying object
+    subscribe() {}
+    //notify() {} // notify happens on the underlying object
+    dispose() {}
+
+}
