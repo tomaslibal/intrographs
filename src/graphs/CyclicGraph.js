9629c3b6534321243be5859567ae69b02438d6df
Tomas Libal <tomas@libal.eu>
2015-08-30 12:49:40 +0100

    cyclic graph factory

---
+++ src/graphs/CyclicGraph.js
@@ -0,0 +1,29 @@
+import { Graph } from "./Graph";
+import { assert } from "../common/assert";
+
+export default class CyclicGraph {
+	
+	constructor(v=3) {
+		assert(v>=3);
+		
+		let c = new Graph();
+		
+		// add vertices	
+		for(let i = 0; i < v; i++) {
+			c.addVertex({'name': `v${i}`});
+		}
+		
+		// add cyclical edges
+		c.vertices.forEach((val, idx, arr) => {
+			if (idx < (arr.length-1)) {
+				// connect the current and the next vertices
+				c.addEdge([`v${idx}`, `v${idx+1}`]);
+			} else {
+				// connect the last and the first vertices
+				c.addEdge([`v0`, `v${idx}`]);
+			}
+		});
+		
+		this.graph = c;
+	}
+}
