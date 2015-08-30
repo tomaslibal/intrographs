f018b83b96e7f4603f0031eb892498a7fdb7d584
Tomas Libal <tomas@libal.eu>
2015-08-30 18:11:12 +0100

    update demo

--- src/demo.js
+++ src/demo.js
@@ -1,23 +1,21 @@
 // interim "main" entry point
 
 import { Graph, getGraph } from "./graphs/Graph";
-import BaseRenderer2D from "./render/BaseRenderer2D";
-import GraphRenderer2D from "./render/GraphRenderer2D";
+import GraphRenderer2D from "./presentation/GraphRenderer2D";
+//import CyclicGraph from "./graphs/CyclicGraph";
+//let C5 = new CyclicGraph(5);
 
-var K3 = getGraph();
+let graphRenderer = new GraphRenderer2D();
+let canvas = document.querySelector("#canvas");
+let ctx = graphRenderer.getContext(canvas);
+let K3 = getGraph();
+
 K3.addVertex({ name: "B" });
 K3.addVertex({ name: "C" });
 
 K3.addEdge(["A", "B"]);
 K3.addEdge(["A", "C"]);
 K3.addEdge(["B", "C"]);
-
-var baseRenderer = new BaseRenderer2D();
-var graphRenderer = new GraphRenderer2D(baseRenderer);
-
-var canvas = document.querySelector("#canvas");
-
-var ctx = baseRenderer.getContext(canvas);
 
 graphRenderer.setCanvas(canvas);
 graphRenderer.render(K3);
