115d3918f40436fa651361c3bf29c9f6d07ec673
Tomas Libal <tomas@libal.eu>
2015-09-02 19:33:14 +0100

    flip the order of identifiers in the assertions

--- test/mathUtilTest.js
+++ test/mathUtilTest.js
@@ -11,23 +11,23 @@
 	
 	 it('returns a random number from the given range', () => {
 		 const result = MathUtil.getRandomArbitrary(0,1);
-		 assert.equal(true, result >= 0);
-		 assert.equal(true, result <= 1);
+		 assert.equal(result >= 0, true);
+		 assert.equal(result <= 1, true);
 	 });
 	 
 	 describe('quadRoots', () => {
                  it('returns null when discriminant < 0', () => {
 		 	const result = MathUtil.quadRoots(1, 5, 100);
-			chai.assert.equal(null, result);
+			chai.assert.equal(result, null);
 		 });
 		 it('returns x=[2] for x^2-4x+4', () => {
 			 const result = MathUtil.quadRoots(1, -4, 4);
-			 chai.assert.deepEqual(result, [2]);
+			 chai.assert.deepEqual([2], result);
 		 });
 
 		 it('returns x=[1, -2] for x^2+x-2', () => {
 			 const result = MathUtil.quadRoots(1, 1, -2);
-			 chai.assert.deepEqual(result, [1, -2]); 
+			 chai.assert.deepEqual([1, -2], result); 
 		 });
 	 });
  });
