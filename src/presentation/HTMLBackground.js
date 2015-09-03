51987e69e8c3084c71415d00ef0a4c8893d1e5ca
Tomas Libal <tomas@libal.eu>
2015-09-03 22:02:13 +0100

    add htmlbackground, htmlgraph, and htmlminidisplay components that will render the models into html document

---
+++ src/presentation/HTMLBackground.js
@@ -0,0 +1,9 @@
+import IRenderable from "./IRenderable";
+
+export default class HTMLBackground extends IRenderable {
+
+	constructor() {
+		super({ 'posX': 0, 'posY': 0 });
+	}
+
+}
---
+++ src/presentation/HTMLGraph.js
@@ -0,0 +1,26 @@
+import IRenderable from "./IRenderable";
+import GraphRenderer2D from "./GraphRenderer2D";
+
+export default class HTMLGraph extends IRenderable {
+
+	constructor(graph) {
+		const x = 0;
+		const y = 0;
+
+		super({ 'posX': x, 'posY': y });
+
+		this.graph = graph;
+		this.x = x;
+		this.y = y;
+	}
+
+	render() {
+		let graphRenderer = new GraphRenderer2D();
+		let canvas = document.querySelector("#canvas");
+		let ctx = graphRenderer.getContext(canvas);
+
+		graphRenderer.setCanvas(canvas);
+		graphRenderer.render(this.graph);
+	}
+
+}
