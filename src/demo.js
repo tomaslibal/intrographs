02a8a129fdd364f4edd7ddfd4b5c3e30575bc528
Tomas Libal <tomas@libal.eu>
2015-09-03 22:03:06 +0100

    use the new HTMLScene class to render everything

--- src/demo.js
+++ src/demo.js
@@ -1,21 +1,20 @@
 // interim "main" entry point
 
 import { Graph, getGraph } from "./graphs/Graph";
-import GraphRenderer2D from "./presentation/GraphRenderer2D";
-//import CyclicGraph from "./graphs/CyclicGraph";
-//let C5 = new CyclicGraph(5);
+import HTMLScene from "./presentation/HTMLScene";
 
-let graphRenderer = new GraphRenderer2D();
-let canvas = document.querySelector("#canvas");
-let ctx = graphRenderer.getContext(canvas);
 let K3 = getGraph();
-
 K3.addVertex({ name: "B" });
 K3.addVertex({ name: "C" });
-
 K3.addEdge(["A", "B"]);
 K3.addEdge(["A", "C"]);
 K3.addEdge(["B", "C"]);
 
-graphRenderer.setCanvas(canvas);
-graphRenderer.render(K3);
+let scene = new HTMLScene(K3);
+scene.renderAll();
+
+// Extra, Prints the graph as an adjacency matrix to the console
+import MatrixPrinter from "./presentation/MatrixPrinter";
+import Matrix from "./common/Matrix";
+let mat = Matrix.fromGraph(K3.getVerticesList(), K3.edges);
+console.log(MatrixPrinter.ascii(mat));
