f5d1ba53bdc4af360ab894ff65b0300b1ec81abc
Tomas Libal <tomas@libal.eu>
2015-09-05 18:52:11 +0100

    HTMLBackground's constructor takes one argument which is the document object

--- src/presentation/HTMLBackground.js
+++ src/presentation/HTMLBackground.js
@@ -2,11 +2,15 @@ import IRenderable from "./IRenderable";
 
 export default class HTMLBackground extends IRenderable {
 
-	constructor() {
+	constructor(documentObj) {
 		super({ 'posX': 0, 'posY': 0 });
 
 		this.height = 480;
 		this.width = 640;
+		this.document = documentObj;
 	}
 
+	render() {
+
+	}
 }
--- test/presentation/htmlBackgroundTest.js
+++ test/presentation/htmlBackgroundTest.js
@@ -8,14 +8,22 @@ let sinon = require("sinon");
 describe('HTMLBackground', () => {
 
 	let bg = null;
+	let mockDocument = {
+
+	};
 
 	beforeEach(() => {
-		bg = new HTMLBackground();
+		bg = new HTMLBackground(mockDocument);
 	});
 
 	it('has properties height and width', () => {
 		chai.assert.property(bg, 'height');
 		chai.assert.property(bg, 'width');
+	});
+
+	it('constructor takes one argument which is the document object', () => {
+		chai.assert.property(bg, 'document');
+		chai.assert.deepEqual(bg.document, mockDocument);
 	});
 
 });
