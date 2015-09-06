1b009945b7ddaf1c26b389048f72e9b839d955af
Tomas Libal <tomas@libal.eu>
2015-09-06 22:17:03 +0100

    implement some features for the demo purposes now

--- src/presentation/ObservableRenderable.js
+++ src/presentation/ObservableRenderable.js
@@ -4,14 +4,24 @@ class ObservableRenderable extends IRenderable {
 
 	constructor({ 'posX': x, 'posY': y }) {
 		super({ 'posX': x, 'posY': y });
+
+		this.callbacks = [];
 	}
 
-	addEventListener() {
-
+	addEventListener(eventType, callback) {
+		this.callbacks.push({ eventType: eventType, callback: callback });
 	}
 
 	removeEventListener() {
 
+	}
+
+	notify(signalEventType, eventObj={}) {
+		this.callbacks.forEach(({ eventType: eventType, callback: callback }) => {
+			if (signalEventType === eventType) {
+				callback.call(undefined, eventObj);
+			}
+		});
 	}
 
 }
