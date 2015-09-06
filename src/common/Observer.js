8bc1e6ad54a7b9338c6b47e13862f6193a8ecc0b
Tomas Libal <tomas@libal.eu>
2015-09-06 15:35:35 +0100

    implement forEach method

--- src/common/Observer.js
+++ src/common/Observer.js
@@ -3,11 +3,14 @@ export default class Observer {
     // observer is created from an object which acts as a source of events
     constructor(source={}, name='untitled observer') {
         if (!source.addEventListener) {
-            throw new Error("The source object does not seem to implement the Observable interface");
+            throw new Error('The source object does not seem to implement the Observable interface');
         }
 
         this.source = source;
         this.name = name;
+
+        this._subscribedEvents = [];
+        this._forEachCallbacks = [];
     }
 
     _handleNewEvent(event) {
@@ -17,10 +20,25 @@ export default class Observer {
     // subscribe, dispose to certain events on the underlying object
     subscribe(eventType) {
         this.source.addEventListener(eventType, this._handleNewEvent);
+        this._subscribedEvents.push(eventType);
     }
     //notify() {} // notify happens on the underlying object
     dispose(eventType) {
         this.source.removeEventListener(eventType, this._handleNewEvent);
+    }
+
+    forEach(callback) {
+        if (this._subscribedEvents.length === 0) {
+            throw new Error('Cannot process forEach because no event has been subscribed');
+            return;
+        }
+
+        if ('function' !== typeof callback) {
+            throw new Error('Callback is not a function');
+            return;
+        }
+
+        this._forEachCallbacks.push(callback);
     }
 
 }
--- test/common/observerTest.js
+++ test/common/observerTest.js
@@ -29,8 +29,14 @@ describe('Observer', () => {
 		it('stores the source object reference so that the client code can later subscribe to events', () => {
 			chai.assert.deepEqual(observer.source, mockSource);
 		});
-		it('sets property name to the value from the second argument', () => {
+		it('sets property "name" to the value from the second argument', () => {
 			chai.assert.equal(observer.name, 'foo42');
+		});
+		it('sets property "_subscribedEvents" to an empty array', () => {
+			chai.assert.deepEqual(observer._subscribedEvents, []);
+		});
+		it('sets property "_forEachCallbacks" to an empty array', () => {
+			chai.assert.deepEqual(observer._forEachCallbacks, []);
 		});
 	});
 
@@ -39,6 +45,15 @@ describe('Observer', () => {
 			observer.subscribe('customEvent');
 
 			assert(mockSource.addEventListener.calledWith('customEvent', sinon.match.func));
+		});
+		it('stores the eventType in property _subscribedEvents', () => {
+			observer.subscribe('minky');
+
+			chai.assert.property(observer, '_subscribedEvents');
+			chai.assert.deepEqual(observer._subscribedEvents, ['minky']);
+
+			observer.subscribe('binky');
+			chai.assert.deepEqual(observer._subscribedEvents, ['minky', 'binky']);
 		});
 	});
 
@@ -49,5 +64,35 @@ describe('Observer', () => {
 			assert(mockSource.removeEventListener.calledWith('customEvent2', sinon.match.func));
 		});
 	});
+
+	describe('forEach', () => {
+		it('throws an Error if no event has been subscribed to and will not add the callback to the _forEachCallbacks array', () => {
+			let fn = () => {
+				observer.forEach(() => {});
+			};
+
+			chai.expect(fn).to.throw(Error);
+			chai.assert.deepEqual(observer._forEachCallbacks, []);
+		});
+		it('stores the callback in _forEachCallbacks array', () => {
+			let callback = sinon.stub();
+
+			observer.subscribe('fusionStart');
+
+			observer.forEach(callback);
+
+			chai.assert.deepEqual(observer._forEachCallbacks, [callback]);
+		});
+		it('throws an Error if callback is not a function and will not add the argument to the _forEachCallbacks array', () => {
+			observer.subscribe('fusionStart');
+
+			let fn = () => {
+				observer.forEach('bar');
+			};
+
+			chai.expect(fn).to.throw(Error);
+			chai.assert.deepEqual(observer._forEachCallbacks, []);
+		});
+	})
 
 });
