a50768ec661159aeb1395c6a759d5ce7742b7e4c
Tomas Libal <tomas@libal.eu>
2015-09-08 21:22:50 +0100

    set the canvas' width and height to window's innerWidth and innerHeight

--- src/presentation/html/HTMLScene.js
+++ src/presentation/html/HTMLScene.js
@@ -3,10 +3,18 @@ import HTMLGraph from "./HTMLGraph";
 import HTMLMinidisplay from "./HTMLMinidisplay";
 import HTMLBackground from "./HTMLBackground";
 import HTMLControls from "./HTMLControls";
+import HTMLWindow from "./HTMLWindow";
 
 export default class HTMLScene extends Scene {
 
-	constructor(graph, document) {
+	constructor(graph=null, windowObj=null) {
+		if (graph === null
+			|| windowObj === null) {
+			throw new Error('supply the constructor with instances of the Graph class and the Window object');
+			return;
+		}
+		let document = windowObj.document;
+
 		let bg = new HTMLBackground(document);
 		let minidisplay = new HTMLMinidisplay();
 		let htmlGraph = new HTMLGraph(graph);
@@ -16,11 +24,24 @@ export default class HTMLScene extends Scene {
 
 		super(htmlGraph, bg, minidisplay, controls);
 
+		this.window = windowObj;
+		this.document = document;
+		this.htmlWindow = new HTMLWindow(this.window);
+
 		this.setupAll();
 	}
 
 	setupAll() {
 		this.graph.setUp();
+		this.setupCanvas();
+	}
+
+	setupCanvas() {
+		let canvas = this.document.querySelector('#canvas');
+		const [width, height] = this.htmlWindow.dims();
+
+		canvas.width = width;
+		canvas.height = height;
 	}
 
 }
--- test/presentation/html/htmlSceneTest.js
+++ test/presentation/html/htmlSceneTest.js
@@ -18,12 +18,52 @@ describe('HTMLScene', () => {
 
 	};
 
+	let mockCanvas = {
+		width: 0,
+		height: 0
+	};
+
 	let mockDocument = {
-		createElement: sinon.stub()
+		createElement: sinon.stub(),
+		querySelector: sinon.stub().returns(mockCanvas)
+	};
+
+	let mockWindow = {
+		document: mockDocument,
+		innerWidth: 640,
+		innerHeight: 480
 	};
 
 	beforeEach(() => {
-		htmlScene = new HTMLScene(mockGraph, mockDocument);
+		htmlScene = new HTMLScene(mockGraph, mockWindow);
+
+		sinon.spy(htmlScene.htmlWindow, 'dims');
+	});
+
+	describe('constructor', () => {
+		it('takes two arguments: an instance of a Graph class and an instance of the Window object', () => {
+			let throws = () => {
+				const htmlSceneThrows = new HTMLScene();
+			};
+			let throwsToo = () => {
+				const htmlSceneThrows = new HTMLScene(graph);
+			};
+			let wontThrow = () => {
+				const htmlSceneGood = new HTMLScene(mockGraph, mockWindow);
+			}
+
+			chai.expect(throws).to.throw(Error);
+			chai.expect(throwsToo).to.throw(Error);
+			chai.expect(wontThrow).not.to.throw();
+		});
+		it('assigns the window object to .window property', () => {
+			chai.assert.property(htmlScene, 'window');
+			chai.assert.deepEqual(htmlScene.window, mockWindow);
+		});
+		it('assigns the document object to .document property', () => {
+			chai.assert.property(htmlScene, 'document');
+			chai.assert.deepEqual(htmlScene.document, mockDocument);
+		});
 	});
 
 	it('uses instances of HTMLBackground, HTMLMinidsplay, HTMLGraph, and HTMLControls', () => {
@@ -32,4 +72,22 @@ describe('HTMLScene', () => {
 		assert(htmlScene.graph instanceof HTMLGraph);
 		assert(htmlScene.controls instanceof HTMLControls);
 	});
+
+	describe('setupCanvas()', () => {
+		it('uses HTMLWindow to set full viewport dimensions on the #canvas element', () => {
+			/*
+			 * The spies are setup after the object construction but setupCanvas()
+			 * is called during construction so the call assertion would not work.
+			 * For this reason the method is invoked again here.
+			 */
+			htmlScene.setupCanvas();
+
+			assert(mockDocument.querySelector.calledWith('#canvas'));
+			assert(htmlScene.htmlWindow.dims.calledOnce);
+
+			chai.assert.equal(mockCanvas.width, mockWindow.innerWidth);
+			chai.assert.equal(mockCanvas.height, mockWindow.innerHeight);
+		});
+	});
+
 });
