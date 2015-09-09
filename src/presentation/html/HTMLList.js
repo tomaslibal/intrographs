9386d63f15959003eef40e18beafdb7db2606333
Tomas Libal <tomas@libal.eu>
2015-09-09 21:57:21 +0100

    a list takes a Document object

---
+++ src/presentation/html/HTMLList.js
@@ -0,0 +1,11 @@
+export default class HTMLList {
+
+	constructor(documentObj=null) {
+		if (documentObj === null) {
+			throw new Error('Constructor must be supplied with the Document object');
+			return;
+		}
+
+		this.document = documentObj;
+	}
+}
---
+++ test/presentation/html/htmlListTest.js
@@ -0,0 +1,39 @@
+// import s.u.t.
+import HTMLList from '../../../src/presentation/html/HTMLList';
+
+let assert = require("assert");
+let chai = require("chai");
+let sinon = require("sinon");
+
+describe('HTMLList', () => {
+
+	let htmlList = null;
+
+	let mockDocument = {
+
+	};
+
+	beforeEach(() => {
+		htmlList = new HTMLList(mockDocument);
+	});
+
+	describe('constructor', () => {
+		it('takes a Document object', () => {
+			let throws = () => {
+				const list = new HTMLList();
+			};
+
+			let wontThrow = () => {
+				const list = new HTMLList(mockDocument);
+			};
+
+			chai.expect(throws).to.throw('Constructor must be supplied with the Document object');
+			chai.expect(wontThrow).not.to.throw();
+		});
+		it('assigns property .document to the supplied document object', () => {
+			chai.assert.property(htmlList, 'document');
+			chai.assert.deepEqual(htmlList.document, mockDocument);
+		});
+	});
+
+});
