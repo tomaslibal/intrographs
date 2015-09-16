62730b9cee6a071dd176056a63c9965063644076
Tomas Libal <tomas@libal.eu>
2015-09-16 21:54:34 +0100

    DFS returns the found node

--- src/search/depthFirstSearch.js
+++ src/search/depthFirstSearch.js
@@ -6,35 +6,52 @@ function dfs(graph, u, path=[]) {
 		ver._dfs_label = 0;
 	});
 
+	// copy the vertex set into 'stack'
 	let stack = graph.vertices.map(ver => {
 		return ver.name;
 	});
 	let found = null;
 
+	// while nodes to be explored > 0 and still have not found the node do:
 	while(stack.length > 0 && found === null) {
 		let v = stack.shift();
+		// run a search of all adjacent nodes recursively
 		found = search(v, graph, u, path);
 	};
+
+	return found;
 }
 
 function search(v, graph, u, path) {
 	let vv = graph.lookupVertex(graph.vertices, v);
 
+	// mark node as "old"
 	vv._dfs_label = 1;
 
 	let adj = graph.adj(v);
 
+	let ret = null;
+
+	// for all adjacent nodes check if one of them is the node to be looked
+	// up. If not, continue searching down the depth of the tree
 	adj.forEach(vName => {
 		let vvv = graph.lookupVertex(graph.vertices, vName);
 
+		// only if marked as "new"
 		if (vvv._dfs_label === 0) {
-
 			path.push([v, vName]);
-			search(vName, graph, u, path);
+
+			// check if found the node
+			if (vName === u) {
+				ret = vvv;
+			} else {
+				// keep searching
+				search(vName, graph, u, path);
+			}
 		}
 	});
 
-	return null;
+	return ret;
 }
 
 export { dfs };
--- test/search/depthFirstSearchTest.js
+++ test/search/depthFirstSearchTest.js
@@ -10,26 +10,33 @@ let assert = require('assert');
 describe('depthFirstSearch', () => {
 
 	let graph;
+	let vertexC;
 
 	beforeEach(() => {
-		 graph = new Graph();
-	});
+		graph = new Graph();
 
-	it('visits D before C to be depth-first', () => {
 		graph.addVertex({ name: "A" });
 		graph.addVertex({ name: "B" });
-		graph.addVertex({ name: "C" });
+		vertexC = graph.addVertex({ name: "C" });
 		graph.addVertex({ name: "D" });
 
 		graph.addEdge(["A", "B"]);
 		graph.addEdge(["A", "C"]);
 		graph.addEdge(["B", "D"]);
+	});
 
+	it('visits D before C to be depth-first', () => {
 		let path = [];
 
 		dfs(graph, 'C', path);
 
 		chai.assert.deepEqual(path, [['A', 'B'], ['B', 'D'], ['A', 'C']]);
+	});
+
+	it('finds the existing vertex C', () => {
+		const r = dfs(graph, 'C');
+
+		chai.assert.deepEqual(r, vertexC);
 	});
 
 });
