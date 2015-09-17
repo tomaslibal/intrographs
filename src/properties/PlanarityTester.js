113886e24e6ee02f4b222fd1e08f5580e10505b4
Tomas (8ba995b) Libal <tomaslibal@users.noreply.github.com>
2015-09-17 08:24:51 +0000

    Added some intro for the vertex addition algorithm

--- src/properties/PlanarityTester.js
+++ src/properties/PlanarityTester.js
@@ -6,6 +6,12 @@
  * or UG (utility graph, also known as K3,3). Although, this algorithm's running time will grow exponentially to the
  * number of edges, because it needs to check every subgraph of whose there are 2^m (m=num. of edges) in every graph
  * (Chiba and Nishizeke, 2008, p. 23).
+ *
+ * This PlanarityTester will employ a vertex addition algorithm described in Chiba and Nishizeke (2008) which uses 
+ * a st-numbering algorithm and a data structure called PQ-tree (Chiba and Nishizeke, 2008, p. 33). St-numbering or
+ * source-sink numbering assigns numbers to vertices such that number 1 (source) and N (sink) (N being the total number of vertices)
+ * are next to each other and also every node has two adjacent nodes such that one adj. node has lower number and 
+ * the other one has a higher number (Moehring, H. R., 1997, p. 357).
  */
  
 export default class PlanarityTester {
