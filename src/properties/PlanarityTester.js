04b129e616d2ae3294b5d12723ef90101f14b7e3
Tomas (8ba995b) Libal <tomaslibal@users.noreply.github.com>
2015-09-17 08:10:23 +0000

    Create PlanarityTester.js

---
+++ src/properties/PlanarityTester.js
@@ -0,0 +1,17 @@
+/*
+ * Planar graph is a graph that can be drawn geometrically on the plane without edge crossings.
+ *
+ * Chiba and Nishizeke (2008, p. 11) give a corollary which says that a planar graph has no vertex of degree > 5. One
+ * could use the Kuratowski's Theorem from which we know that a graph is planar if it does not contain subgraphs of K5
+ * or UG (utility graph, also known as K3,3). Although, this algorithm's running time will grow exponentially to the
+ * number of edges, because it needs to check every subgraph of whose there are 2^m (m=num. of edges) in every graph
+ * (Chiba and Nishizeke, 2008, p. 23).
+ */
+ 
+export default class PlanarityTester {
+
+    static test(graph) {
+    
+    }
+
+}
