b18ae64d7165e4f1cacf2eb720885607828b821d
Tomas Libal <tomas@libal.eu>
2015-09-18 21:47:50 +0100

    add uniqueValues and addIfNotFound functions for working with arrays

--- src/common/eloquent.js
+++ src/common/eloquent.js
@@ -7,3 +7,28 @@ export function unique(arr) {
            }
         });
 }
+
+export function addIfNotFound(array, member) {
+    if (array.indexOf(member) === -1) {
+        return array.push(member);
+    } else {
+        return array;
+    }
+}
+
+export function uniqueValues(array) {
+    let uniques = [];
+
+    array.forEach(member => {
+        if (member.constructor.name === 'Array') {
+            let mems = uniqueValues(member);
+            mems.forEach(m => {
+                addIfNotFound(uniques, m);
+            });
+        } else {
+            addIfNotFound(uniques, member);
+        }
+    });
+
+    return uniques;
+}
