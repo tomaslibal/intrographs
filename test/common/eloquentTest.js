741b7639930cf3ce4fcb0b9562c1ab1d76de1e52
Tomas Libal <tomaslibal@users.noreply.github.com>
2015-09-25 08:21:12 +0000

    add placeholder for a test case

---
+++ test/common/eloquentTest.js
@@ -0,0 +1,14 @@
+// import s.u.t.
+import {
+    unique,
+    addIfNotFound,
+    uniqueValues
+    }
+  from '../../src/common/eloquent';
+  
+let chai = require('chai');
+let sinon = require('sinon');
+
+describe('eloquent module', () => {
+
+});
