06b337e7e0012d65e6af52ee1cf83b8f76a4d0e6
Tomas Libal <tomas@libal.eu>
2015-09-26 14:26:31 +0100

    add comment with help

--- dev/createNewDescribe.sh
+++ dev/createNewDescribe.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Creates a new JavaScript test case file for the Mochajs test runner and test framework.
+#
+# Usage:
+#
+#    for a module name src/properties/PlanarityTester.js
+#    executed the script as > ./createNewDescribe.sh properties/planarityTester
+#
+#    this will create a file test/properties/planarityTesterTest.js
+#    with the boilerplate from the below here-document.
+#
+
 name=${1:-'defaultModule'}
 output=test/${name}Test.js
 
