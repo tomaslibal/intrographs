d22cc23678aa11d95d19d62f984051d896355b90
Tomas Libal <tomaslibal@users.noreply.github.com>
2015-09-29 07:39:41 +0000

    add optional msg to the assertion as a second param

--- src/common/assert.js
+++ src/common/assert.js
@@ -1,7 +1,12 @@
-export default function assert(stmt) {
+/*
+ * stmt bool: statement to be asserted as true
+ * msg string: message to be shown if the assertion fails
+ * return void
+ */
+export default function assert(stmt, msg) {
     if (stmt) { return; }
 
-    throw new Error("Assertion Error!");
+    throw new Error("Assertion Error! " + msg);
 }
 
 // alias on the default named export
