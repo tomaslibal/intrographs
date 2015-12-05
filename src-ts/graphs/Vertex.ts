20f4ab901d4e0790fb3232eb4311dab1ac54973f
Tomas Libal <tomas@libal.eu>
2015-12-05 18:27:08 +0000

    add vertex position into the vertex class

--- src-ts/graphs/Vertex.ts
+++ src-ts/graphs/Vertex.ts
@@ -8,11 +8,18 @@ export interface VertexProperties {
     name?: string;
 }
 
+export interface VertexPosition2D {
+    x: number;
+    y: number;
+}
+
 export class Vertex {
 
     id:     string;
     name:   string;
     symbol: Symbol;
+
+    position: VertexPosition2D;
 
     constructor(p: VertexProperties) {
 
@@ -28,5 +35,13 @@ export class Vertex {
         this.name = p.name || null;
 
         this.symbol = Symbol(this.name);
+    }
+
+    setPosition(p: VertexPosition2D) {
+        this.position = p;
+    }
+
+    getPosition(): VertexPosition2D {
+        return this.position;
     }
 }
