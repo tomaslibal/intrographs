96a55d3673bf3221210716753965f22513f0400e
Tomas Libal <tomas@libal.eu>
2015-12-08 19:58:25 +0000

    mirror the main from src/main.js

--- src-ts/main.ts
+++ src-ts/main.ts
@@ -2,3 +2,17 @@
  * INTROGRAPHS 2.0.0-pre
  */
 
+import { HTMLScene } from 'presentation/Scene';
+import { Graph } from 'graphs/Graph';
+
+let K3 = new Graph();
+K3.addVertex({ id: "B" });
+K3.addVertex({ id: "C" });
+K3.addEdge(["A", "B"]);
+K3.addEdge(["A", "C"]);
+K3.addEdge(["B", "C"]);
+
+function init() {
+	let scene = new HTMLScene(K3, window);
+	scene.render();
+}
