836edcefbecf85bc9e678b70ef56bf0c7100fea3
Tomas Libal <tomas@libal.eu>
2015-12-08 19:58:12 +0000

    add basic canvas settings

--- src-ts/presentation/Canvas.ts
+++ src-ts/presentation/Canvas.ts
@@ -1,3 +1,32 @@
+export interface CanvasSettings {
+	width: number;
+	height: number;
+	context?: string;
+}
+
 export class Canvas {
 
+	private canvas: HTMLCanvasElement;
+	private context: CanvasRenderingContext2D;
+	private settings: CanvasSettings;
+
+	constructor(canvas: HTMLCanvasElement, settings?: CanvasSettings) {
+		this.canvas = canvas;
+
+		this.context = this.canvas.getContext("2d");
+
+		if (settings) {
+			this.config(settings);
+		}
+	}
+
+	config(settings: CanvasSettings) {
+		this.settings = settings;
+		this.applySettings();
+	}
+
+	private applySettings() {
+		this.canvas.width = this.settings.width;
+		this.canvas.height = this.settings.height;
+	}
 }
