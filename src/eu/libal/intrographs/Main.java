2711245e2504191feaf8fb634c3d851943b87e7b
Tomas Libal <tomas@libal.eu>
2016-02-09 21:29:19 +0000

    add main()

---
+++ src/eu/libal/intrographs/Main.java
@@ -0,0 +1,11 @@
+package eu.libal.intrographs;
+
+/**
+ * Created by tlibal on 2/9/16.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("INTROGRAPHS2");
+    }
+}
