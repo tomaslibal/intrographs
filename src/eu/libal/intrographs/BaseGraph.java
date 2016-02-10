8baaf7ba2640b27eebb2acd74bb64b751ff2e475
Tomas Libal <tomas@libal.eu>
2016-02-10 21:40:24 +0000

    add getNewId member function

--- src/eu/libal/intrographs/BaseGraph.java
+++ src/eu/libal/intrographs/BaseGraph.java
@@ -1,8 +1,18 @@
 package eu.libal.intrographs;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.Set;
 
 abstract public class BaseGraph<VertexType, EdgeClass> implements IGraph<VertexType, EdgeClass> {
     protected Set<Vertex<VertexType>> vertices;
     protected Set<EdgeClass> edges;
+
+    /**
+     *
+     * @return new generated String id
+     */
+    protected String getNewId() {
+        return Date.from(Instant.now()).toString();
+    }
 }
