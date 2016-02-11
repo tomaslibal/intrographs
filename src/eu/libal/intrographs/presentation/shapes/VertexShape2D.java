ada42e7665129916506a4ceafa135135d3ef717b
Tomas Libal <tomas@libal.eu>
2016-02-11 20:46:49 +0000

    add a paint method body to render a circle

--- src/eu/libal/intrographs/presentation/shapes/VertexShape2D.java
+++ src/eu/libal/intrographs/presentation/shapes/VertexShape2D.java
@@ -1,14 +1,20 @@
 package eu.libal.intrographs.presentation.shapes;
 
+import javafx.scene.paint.Color;
+
 /**
  *
  */
-public class VertexShape extends BasicRenderableShape {
+public class VertexShape2D extends BasicShape2D {
 
     @Override
     public void paint() throws Exception {
         if (ctx == null) {
             throw new Exception("No GraphicsContext");
         }
+
+        ctx.setFill(new Color(0, 0, 0.99, 1));
+        ctx.fillOval(getX(), getY(),  getWidth(), getHeight());
     }
+
 }
