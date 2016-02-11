6b3cc3c69ddd94bb53a009ee719a7aedaecfa309
Tomas Libal <tomas@libal.eu>
2016-02-11 20:16:44 +0000

    This is an interface that should be implemented by any object that wants to be renderable

---
+++ src/eu/libal/intrographs/presentation/shapes/IRenderableShape.java
@@ -0,0 +1,11 @@
+package eu.libal.intrographs.presentation.shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ *
+ */
+public interface IRenderableShape {
+    void setContext(GraphicsContext ctx);
+    void paint();
+}
