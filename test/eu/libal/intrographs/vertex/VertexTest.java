0e738b8510a9e9772136262a17493a1be5de99c2
Tomas Libal <tomas@libal.eu>
2016-02-11 13:30:25 +0000

    add tests for getId and getValue methods

---
+++ test/eu/libal/intrographs/vertex/VertexTest.java
@@ -0,0 +1,23 @@
+package eu.libal.intrographs.vertex;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class VertexTest {
+
+    @Test
+    public void shouldReturnTheStoredValue_WhenGetValueInvoked() throws Exception {
+        Vertex<Integer> v1 = new Vertex<>(42, "foo");
+
+        assertThat(v1.getValue(), is(42));
+    }
+
+    @Test
+    public void shouldReturnTheID_WhenGetIdInvoked() throws Exception {
+        Vertex<Integer> v1 = new Vertex<>(42, "foo");
+
+        assertThat(v1.getId(), is("foo"));
+    }
+}
