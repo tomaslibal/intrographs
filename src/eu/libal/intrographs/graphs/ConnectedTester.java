af504dff58b1dc8e12953865cca1ddd9cf39bb5e
Tomas Libal <tomas@libal.eu>
2016-02-17 13:36:06 +0000

    add a skeleton for the connectedness tester

---
+++ src/eu/libal/intrographs/graphs/ConnectedTester.java
@@ -0,0 +1,13 @@
+package eu.libal.intrographs.graphs;
+
+public class ConnectedTester {
+
+    public static boolean isConnected(Graph g) {
+        return false;
+    }
+
+    public static int getNumberOfComponents(Graph g) {
+        return -1;
+    }
+
+}
