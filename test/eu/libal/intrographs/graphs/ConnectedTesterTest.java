131b177fdbf41130d101b9d9f03f3f05df7d82ed
Tomas Libal <tomas@libal.eu>
2016-02-17 13:54:16 +0000

    add a test for the connectedness tester

---
+++ test/eu/libal/intrographs/graphs/ConnectedTesterTest.java
@@ -0,0 +1,33 @@
+package eu.libal.intrographs.graphs;
+
+import eu.libal.intrographs.graphs.edge.Edge;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class ConnectedTesterTest {
+
+    private Graph<Integer, Edge<Integer>> g;
+
+    @Before
+    public void setup() {
+        g = new Graph<Integer, Edge<Integer>>();
+    }
+
+    @Test
+    public void shouldReturnTrueIfAllVerticesAreConnected() {
+        g.addVertex(0, "a");
+        g.addVertex(1, "b");
+        g.addVertex(2, "c");
+        g.addVertex(3, "d");
+
+        g.addEdge("a", "b");
+        g.addEdge("a", "c");
+        g.addEdge("c", "d");
+
+        assertThat(ConnectedTester.isConnected(g), is(true));
+    }
+
+}
