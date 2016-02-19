35c096093f354c2f59dccdbd3eb1d6ba23af7794
Tomas Libal <tomas@libal.eu>
2016-02-19 22:22:20 +0000

    optimize code

--- test/eu/libal/intrographs/graphs/vertex/VertexTest.java
+++ test/eu/libal/intrographs/graphs/vertex/VertexTest.java
@@ -1,7 +1,6 @@
 package eu.libal.intrographs.graphs.vertex;
 
 import org.junit.Test;
-import org.mockito.internal.util.collections.Sets;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -10,21 +9,21 @@ import static org.mockito.internal.util.collections.Sets.newSet;
 public class VertexTest {
 
     @Test
-    public void shouldReturnTheStoredValue_WhenGetValueInvoked() throws Exception {
+    public void shouldReturnTheStoredValue_WhenGetValueInvoked() {
         Vertex<Integer> v1 = new Vertex<>(42, "foo");
 
         assertThat(v1.getValue(), is(42));
     }
 
     @Test
-    public void shouldReturnTheID_WhenGetIdInvoked() throws Exception {
+    public void shouldReturnTheID_WhenGetIdInvoked() {
         Vertex<Integer> v1 = new Vertex<>(42, "foo");
 
         assertThat(v1.getId(), is("foo"));
     }
 
     @Test
-    public void shouldCreateVertexWithNoAdjacentNodes() throws Exception {
+    public void shouldCreateVertexWithNoAdjacentNodes() {
         Vertex<Integer> v = new Vertex<>(42, "foo");
 
         assertThat(v.getAdjacentVertices().size(), is(0));
