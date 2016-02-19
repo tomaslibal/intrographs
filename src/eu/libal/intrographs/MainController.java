3f1a717a6e069514a6e108fcd93a9e8d15dd5584
Tomas Libal <tomas@libal.eu>
2016-02-19 13:41:31 +0000

    add "addEdge" button and add a method that selects a node on click

--- src/eu/libal/intrographs/MainController.java
+++ src/eu/libal/intrographs/MainController.java
@@ -2,8 +2,10 @@ package eu.libal.intrographs;
 
 import eu.libal.intrographs.graphs.Graph;
 import eu.libal.intrographs.graphs.edge.Edge;
+import eu.libal.intrographs.graphs.vertex.IVertex;
 import eu.libal.intrographs.presentation.CanvasStates;
 import eu.libal.intrographs.presentation.GraphRenderer;
+import eu.libal.intrographs.presentation.shapes.VertexShape2D;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,6 +19,7 @@ import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -26,6 +29,9 @@ public class MainController implements Initializable {
 
     @FXML
     public Button addVertexBt;
+
+    @FXML
+    public Button addEdgeBt;
 
     @FXML
     private Canvas mainCanvas;
@@ -88,11 +94,43 @@ public class MainController implements Initializable {
             canvasState = CanvasStates.PANNING;
             addVertexBt.setText("Add vertex");
         }
+
+        Optional<VertexShape2D> selectedVertex = getVertexAtMouseClick(event);
+
+        if (selectedVertex.isPresent()) {
+            System.out.println("found vertex " + selectedVertex.get().getVertexId());
+        }
     }
 
     @FXML
     public void setCanvasStatusToAddingVertex(ActionEvent actionEvent) {
         addVertexBt.setText("Click on canvas");
         canvasState = CanvasStates.ADDING_VERTEX;
+    }
+
+    @FXML
+    public void setCanvasStatusToAddingEdge(ActionEvent actionEvent) {
+        addEdgeBt.setText("Click on edge");
+        canvasState = CanvasStates.ADDING_EDGE;
+    }
+
+    private Optional<VertexShape2D> getVertexAtMouseClick(MouseEvent click) {
+
+        int x = (int) Math.round(click.getX());
+        int y = (int) Math.round(click.getY());
+
+        int leniency = 5;
+        int radius = 5;
+
+        Optional<VertexShape2D> vertexAtClickPos = graphRenderer.getVertexShapes().stream()
+                .filter(shape -> {
+                    int dx = Math.abs(shape.getX() - x);
+                    int dy = Math.abs(shape.getY() - y);
+
+                    return dx < (leniency + radius) && dy < (leniency + radius);
+                })
+                .findFirst();
+
+        return vertexAtClickPos;
     }
 }
