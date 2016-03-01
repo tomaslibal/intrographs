24e5ce727a78a47479913ee2453b8a8ecc00365e
Tomas Libal <tomas@libal.eu>
2016-03-01 13:56:09 +0000

    add print method

--- src/eu/libal/intrographs/graphlang/IParser.java
+++ src/eu/libal/intrographs/graphlang/IParser.java
@@ -15,4 +15,14 @@ public interface IParser {
      * @return a parse tree representing the input sentence as a graph
      */
     Graph<BasicOperation, Edge<BasicOperation>> parse(String sentence);
+
+    /**
+     * Given a parse tree this method prints its string representation. This
+     * is a reverse operation of {@code parse}. Some information like whitespace
+     * is lost during parsing so this is not preserved.
+     *
+     * @param parseTree obtained from parsing a string
+     * @return a string that would produce the same parse tree if parsed
+     */
+    String print(Graph<BasicOperation, Edge<BasicOperation>> parseTree);
 }
