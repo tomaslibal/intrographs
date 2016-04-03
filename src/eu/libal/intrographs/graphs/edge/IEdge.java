db542959f49dd674ae8092aa4f11af23f12fbace
Tomas Libal <tomas@libal.eu>
2016-04-03 10:24:29 +0100

    Remove the default comment

--- src/eu/libal/intrographs/graphs/edge/IEdge.java
+++ src/eu/libal/intrographs/graphs/edge/IEdge.java
@@ -3,11 +3,7 @@ package eu.libal.intrographs.graphs.edge;
 import eu.libal.intrographs.graphs.vertex.IVertex;
 import eu.libal.intrographs.graphs.vertex.Vertex;
 
-/**
- * Created by tlibal on 2/9/16.
- */
-public interface IEdge<T>
-{
+public interface IEdge<T> {
     IVertex<T> getTarget();
 
     IVertex<T> getSource();
