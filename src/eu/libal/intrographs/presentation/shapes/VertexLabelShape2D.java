2299066a2dea9138ed39e8a3bdd2b84be617f223
Tomas Libal <tomas@libal.eu>
2016-04-17 15:03:40 +0100

    Adds test for the VertexLabelShape auto-update of its coordinates when the bound vertexShape coordinates change

--- src/eu/libal/intrographs/presentation/shapes/VertexLabelShape2D.java
+++ src/eu/libal/intrographs/presentation/shapes/VertexLabelShape2D.java
@@ -5,8 +5,12 @@ import java.io.Serializable;
 public class VertexLabelShape2D extends TextShape2D implements Serializable {
     private final VertexShape2D<?> vertexShape;
 
+    public static final double X_OFFSET = -5.0;
+    public static final double Y_OFFSET = 20.0;
+
     public VertexLabelShape2D(VertexShape2D<?> vertexShape) {
         this.vertexShape = vertexShape;
+        updateCoords("init");
         subscribe();
     }
 
@@ -15,7 +19,7 @@ public class VertexLabelShape2D extends TextShape2D implements Serializable {
     }
 
     private void updateCoords(String msg) {
-        setX(vertexShape.listenableCoords.getValue().getX() - 5);
-        setY(vertexShape.listenableCoords.getValue().getY() + 20);
+        setX(vertexShape.listenableCoords.getValue().getX() + X_OFFSET);
+        setY(vertexShape.listenableCoords.getValue().getY() + Y_OFFSET);
     }
 }
---
+++ test/eu/libal/intrographs/presentation/shapes/VertexLabelShape2DTest.java
@@ -0,0 +1,59 @@
+package eu.libal.intrographs.presentation.shapes;
+
+import eu.libal.intrographs.common.ListenableField;
+import eu.libal.intrographs.graphs.vertex.Vertex;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+
+public class VertexLabelShape2DTest {
+
+    @Spy
+    VertexShape2D<Integer> vertexShape;
+
+    @Spy
+    ListenableField<Coordinates2D> listenableCoords;
+
+    VertexLabelShape2D labelShape;
+
+    private int vertex_x = 10;
+
+    private int vertex_y = 10;
+
+    @Before
+    public void setup() {
+        VertexShape2D<Integer> vertexShape2D = new VertexShape2D<>(vertex_x, vertex_y, new Vertex<>("vertexShape2D"));
+        listenableCoords = Mockito.spy(vertexShape2D.listenableCoords);
+        vertexShape = Mockito.spy(vertexShape2D);
+
+        // Swap the listenableCoords field with the Spy
+        vertexShape.listenableCoords = listenableCoords;
+        labelShape = new VertexLabelShape2D(vertexShape);
+    }
+
+    @Test
+    public void shouldSubscribeToUpdatesFromTheCoordinatesField() {
+        verify(listenableCoords).subscribe(eq("update"), any());
+    }
+
+    @Test
+    public void shouldUpdateItsCoordinatesWhenVertexShapeUpdatesCoordinates() {
+        VertexLabelShape2D spy = Mockito.spy(labelShape);
+
+        assertThat(labelShape.getX(), is(vertex_x + VertexLabelShape2D.X_OFFSET));
+        assertThat(labelShape.getY(), is(vertex_y + VertexLabelShape2D.Y_OFFSET));
+
+        vertexShape.setX(20.0);
+        vertexShape.setY(40.0);
+
+        assertThat(labelShape.getX(), is(20.0 + VertexLabelShape2D.X_OFFSET));
+        assertThat(labelShape.getY(), is(40.0 + VertexLabelShape2D.Y_OFFSET));
+    }
+}
