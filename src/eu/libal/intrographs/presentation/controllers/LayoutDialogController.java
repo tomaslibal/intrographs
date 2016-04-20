37d51847f05388d1c848a1eeff6aa71e7634f978
Tomas Libal <tomas@libal.eu>
2016-04-20 18:42:33 +0100

    adds an empty dialog with its controller
    will be used for adjusting layout algorithm settings

---
+++ src/eu/libal/intrographs/presentation/controllers/LayoutDialogController.java
@@ -0,0 +1,14 @@
+package eu.libal.intrographs.presentation.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LayoutDialogController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
